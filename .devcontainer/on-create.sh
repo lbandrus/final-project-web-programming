25d78c230f943dd92eb2d1d3aa253c3c548e5704
Logan Andrus <77907830+lbandrus@users.noreply.github.com>
2024-04-12 00:08:57 -0600

    reverted changes after trying to publish

--- .devcontainer/on-create.sh
+++
@@ -1,5 +0,0 @@
-# Override the .zshrc file with config/dot.zshrc
-cp .devcontainer/config/dot.zshrc ~/.zshrc
-
-# Override the .gitconfig file with config/dot.gitconfig
-cp .devcontainer/config/dot.gitconfig ~/.gitconfig
